b755c5c87a47b71f7289c27f6a5f90e320165f74
Yeshi <tyeshi777@yahoo.com>
2019-10-03 17:49:51 -0700

    added useRedux file to get the redux state and dispatch function with useRedux

---
+++ useRedux.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useRedux } from "use-redux";
+
+export const Counter = () => {
+  const [state, dispatch] = useRedux();
+
+  useEffect(() => {
+    const timeOut = setTimeout(
+      () => dispatch({ type: "SET", count: state.count + 1 }),
+      1000
+    );
+    return () => clearTimeout(timeOut);
+  }, []);
+  return state.count;
+};
+
+export default Counter;
